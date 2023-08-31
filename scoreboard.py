f797b4259de9d835c400f2a6a8efd6756558c18c
Netanel Ashtam <netanel.ashtam@888holdings.com>
2023-08-31 11:52:52 +0300

    complete snake game

---
+++ scoreboard.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0,265)
+        self.write (f"Score: {self.score}",align="center",font=("Arial",24,"normal"))
+        self.hideturtle()
+      
+    def increas_score(self):
+        self.clear()
+        self.score += 1
+        self.write (f"Score: {self.score}",align="center",font=("Arial",24,"normal"))
+
+    def game_over(self): 
+        self.goto(0,0)
+        self.write ("GAME OVER!",align="center",font=("Arial",24,"normal"))
